492cbc1f1bcfec9442c57023e71f62a7021b6491
Guilherme Chaves <guilherme.clc17@gmail.com>
2022-11-19 18:45:47 -0300

    algumas alterações visuas e inicio da class produtos

--- projeto_main.py
+++ projeto_main.py
@@ -1,6 +1,12 @@
+class Produtos:
+    def __init__(self, cod):
+        self.cod = cod
+
 def main():
     print("Bem-vindo a LearnStore!")
-def add():
+    menu_principal()
+
+def add(aux):
     pass
 
 def remove():
@@ -10,24 +16,28 @@ def finish():
     pass
 
 def cancel():
-    pass
-
+    print("\nSua compra foi cancelada! Obrigado por nos visitar, até a próxima!\n")
 
 def menu_principal():
     x = True
     while x == True:
-        option = int(input("Escolha uma opção: 0 ➙ Cancel, 1 ➙ ADD, 2 ➙ REMOVE, 3 ➙ FINISH\n"))
+        option = int(input("Escolha uma opção (numero inteiro):\n0 ➙ CANCEL;\n1 ➙ ADD;\n2 ➙ REMOVE;\n3 ➙ FINISH.\nPor favor, insira o código: "))
         match option:
             case 0:
                 cancel()
                 x = False
             case 1:
                 add()
+                pass
             case 2:
                 remove()
+                pass
             case 3:
                 finish()
+                x = False
+            case default:
+                print("\nCódigo inválido! Por favor, insira o código correspondente.\n")
+                pass
 
 if __name__ == "__main__":
     main()
-    menu_principal()
