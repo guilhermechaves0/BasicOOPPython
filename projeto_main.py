cf29345b8074cdc2c38323e216d6865848cc02a2
Deyves Oliveira <cristian_deyvinho@hotmail.com>
2022-11-20 09:04:51 -0300

    add novos produtos

--- projeto_main.py
+++ projeto_main.py
@@ -1,22 +1,13 @@
+import random
+
 class Produtos:
 
+    produtos = []
+    
     def __init__(self, product, quantity, price):
         self.product = str(product)
         self.quantity = int(quantity)
         self.price = float(price)
-
-def main():
-    print("Bem-vindo a LearnStore!")
-    #   menu_principal()
-    
-    #   Testando a inserir coisas a lista
-    produtos = []
-    produtos.append(Produtos('Maçã', 10, 2.58))
-    produtos.append(Produtos('Laranja', 10, 5.98))
-    produtos.append(Produtos('Manga', 10, 4.23))
-    
-    for i in produtos:
-        print(i.product, i.quantity, i.price)
 
 def add(aux):
     pass
@@ -29,8 +20,14 @@ def finish():
 
 def cancel():
     print("\nSua compra foi cancelada! Obrigado por nos visitar, até a próxima!\n")
+    
+def add_product(list, product):
+    list = []
+    quantity = 15
+    price = random.uniform(3, 9.9)
+    list.append(Produtos(product, quantity, price))
 
-def menu_principal():
+def menu_principal(list):
     x = True
     while x == True:
         option = int(input("Escolha uma opção (numero inteiro):\n0 ➙ CANCEL;\n1 ➙ ADD;\n2 ➙ REMOVE;\n3 ➙ FINISH.\nPor favor, insira o código: "))
@@ -49,11 +46,26 @@ def menu_principal():
                 x = False
             case 4:
                 #   Adicionar produtos
-                #   Usar o random.uniform(a, b)
+                add_product(list, produto = input("Digite o nome do produto a ser adicionado: "))
                 pass
             case default:
                 print("\nCódigo inválido! Por favor, insira o código correspondente.\n")
                 pass
+            
+def main():
+    listCliente = []
+    print("Bem-vindo a LearnStore!")
+    menu_principal(listCliente)
+    
+    #   Testando a inserir coisas a lista
+    produtos = []
+    produtos.append(Produtos('Maçã', 10, 2.58))
+    produtos.append(Produtos('Laranja', 10, 5.98))
+    produtos.append(Produtos('Manga', 10, 4.23))
+    
+    for i in produtos:
+        print(i.product, i.quantity, i.price)
+
 
 if __name__ == "__main__":
     main()
